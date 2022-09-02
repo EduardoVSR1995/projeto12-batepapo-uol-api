7f0f5c280efab6e3e406f81d22bfcfa5b64b4c06
EduardoVSR1995 <eduardvitor7@gmail.com>
2022-09-02 16:12:23 -0300

    feat: post de mensagens

--- assets/server/server.js
+++ assets/server/server.js
@@ -10,98 +10,100 @@ const mongoClient = new MongoClient(process.env.SERVER_URI)
 
 let db;
 
-let user;
-
 mongoClient.connect().then(() => { db = mongoClient.db("participants"); });
 
 const server = express();
 server.use(cors());
 server.use(express.json());
 
-function getArreyDB(){
+function getArreyDB(value) {
     const list1 = db.collection("messages").find().toArray();
     const list2 = db.collection("participants").find().toArray();
-    
-    return [list1,list2] ;
+
+    return [list1, list2];
 }
 async function insertObj(value) {
     await db.collection("messages").insertOne(value);
 }
 
 
-server.get("/participants", async (req,res)=>{
+server.get("/participants", async (req, res) => {
     const list = await getArreyDB()[1];
     return res.send(list).status(200);
-} );
+});
 
 
-server.get("/messages", async (req,res)=>{ 
+server.get("/messages", async (req, res) => {
+    try{
     const limit = Number(req.query.limit);
-    
+
     const list = await getArreyDB()[0];
-    
-    res.send(list.slice(-limit));  
+
+    return res.send(list.slice(-limit));
+}catch(error){
+    return res.sendStatus(401);
+}
 
 });
 
-server.post('/messages', (req,res)=>{
-    const userSchema = joi.object({to:joi.string().required(), text:joi.string().required(), type:joi.string().required()})
-    
-    const validation  = userSchema.validate(req.body);
+server.post('/messages', (req, res) => {
+    try {
+        const userSchema = joi.object({ to: joi.string().required(), text: joi.string().required(), type: joi.string().valid("message", "private_message").required() })
+        const validation = userSchema.validate(req.body);
+        if (validation.error) {
+            return res.sendStatus(422);
+        }
+        const objUser = {
+            from: req.headers.user,
+            to: req.body.to,
+            text: req.body.text,
+            type: req.body.type,
+            time: dayjs().format('HH:mm:ss')
+        }
 
-    if(validation.error){
+        insertObj(objUser);
+        return res.sendStatus(201)
+    } catch (error) {
         return res.sendStatus(422);
     }
-    const objUser = {
-        from: user.name,
-        to: req.body.to,
-        text: req.body.text,
-        type: req.body.type,
-        time: dayjs().format('HH:mm:ss')
-    }       
-
-    insertObj(objUser);
-    res.sendStatus(201)
 })
 
 
-server.post('/participants', async (req,res)=>{    
+server.post('/participants', async (req, res) => {
+    try {
+        const name = req.body.name;
 
-    const name = req.body.name;
-    
-    const userSchema = joi.object({name:joi.string().required()})
-    
-    const validation  = userSchema.validate(req.body, { abortEarly: true });
+        const userSchema = joi.object({ name: joi.string().required() })
 
-    if(validation.error){
-        return res.sendStatus(422);
-    }
-    
-    const list = await getArreyDB()[1];
-    const check = list.filter((value)=> `${value.name}`.toUpperCase() === `${name}`.toUpperCase())
+        const validation = userSchema.validate(req.body, { abortEarly: true });
 
-    if(check.length>0){
-        return res.sendStatus(409);
-    }
-    
-    const objUser = {
-        from: name,
-        to: 'Todos',
-        text: 'entra na sala...',
-        type: 'status',
-        time: dayjs().format('HH:mm:ss')
-    }
-    
-    try{
-        await db.collection("participants").insertOne({name: name, lastStatus: Date.now() });
+        if (validation.error) {
+            return res.sendStatus(422);
+        }
 
-        user = await db.collection("participants").findOne(req.body);
+        const list = await getArreyDB()[1];
+        const check = list.filter((value) => `${value.name}`.toUpperCase() === `${name}`.toUpperCase())
+
+        if (check.length > 0) {
+            return res.sendStatus(409);
+        }
+
+        const objUser = {
+            from: name,
+            to: 'Todos',
+            text: 'entra na sala...',
+            type: 'status',
+            time: dayjs().format('HH:mm:ss')
+        }
+
+
+        await db.collection("participants").insertOne({ name: name, lastStatus: Date.now() });
 
         insertObj(objUser);
-    
+
         return res.sendStatus(201);
-    
-    }catch(error){
+
+    } catch (error) {
         return res.sendStatus(422);
     }
 
